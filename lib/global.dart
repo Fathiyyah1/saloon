b5622f0e78931c83fc258a2372bcd13b876df5b6
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-06-10 00:33:19 +0300

    .

--- lib/global.dart
+++ lib/global.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 
 class AppColors {
-  static Color? primary = const Color(0xFFC10000);
+  static Color? primary = Color.fromARGB(255, 169, 93, 137);
+  static Color? primary1 = Color.fromARGB(255, 154, 100, 131);
 }
