95dbe4d47f2468fee4c8add907aba9082e330105
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-06-10 12:21:03 +0300

    models

---
+++ lib/models/expense.dart
@@ -0,0 +1,12 @@
+class Expense {
+  Expense({
+    required this.id,
+    required this.date,
+    required this.amount,
+    required this.name,
+  });
+  String id;
+  double amount;
+  String name;
+  DateTime date;
+}
