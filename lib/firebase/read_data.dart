929b08b8e151a46cc820bd6dae1bba42906046b1
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-06-10 22:11:04 +0300

    .

--- lib/firebase/read_data.dart
+++
@@ -1,6 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-Future getUser(String username) async {
-  final doc = FirebaseFirestore.instance.collection('users').get();
-  print(doc);
-}
--- lib/firebase/write_data.dart
+++
@@ -1,6 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-Future putUser(String username) async {
-  final doc = FirebaseFirestore.instance.collection('users');
-  print(doc);
-}
